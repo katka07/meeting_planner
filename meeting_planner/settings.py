d7840d0b1f502444f5451f23f671a53d95a2d110
katka07 <katka07@yahoo.com>
2023-07-01 23:37:42 +0530

    Added IP in ALLOWED_HOSTS

--- meeting_planner/settings.py
+++ meeting_planner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@n$nd56b5+n0oq+-i96b+!51)-cv$!5piw$pv(nk^gq#lsucel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
