a2a3804ad15d78b2f2203df68fff980d3dc02d4c
katka07 <katka07@yahoo.com>
2023-10-01 14:27:09 +0530

    Added MySQL support - 01OCT2023

--- meeting_planner/settings.py
+++ meeting_planner/settings.py
@@ -75,23 +75,23 @@ NOSE_ARGS = [
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
         'NAME': 'meetings',
         'USER': 'admin',
         'PASSWORD': 'firewall',
-        'HOST': 'localhost', 
+        'HOST': 'mysqldb', 
         'PORT': '3306', 
     }
-} """
+}
 
 
 # Password validation
