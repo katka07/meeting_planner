aad03c378d0cef0d1990f161381258b1dc077588
katka07 <katka07@yahoo.com>
2023-10-01 12:59:42 +0530

    Added host and container names to page - 01OCT2023

--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import socket, os
 
 from meetings.models import Meeting
 
 
 def welcome(request):
+    node_name = socket.gethostname()
+    container_name = os.environ.get("HOSTNAME")
     return render(request, "website/welcome.html",
-    {"meetings": Meeting.objects.all()})
+    {"node_name": node_name, "container_name" : container_name, "meetings": Meeting.objects.all()})
