bbcd1d3b88cdea6bd31be2e88852f30e757d873d
katka07 <katka07@yahoo.com>
2024-01-03 04:01:58 +0530

    Fixed proper hostname display - 02JAN2024

--- website/views.py
+++ website/views.py
@@ -6,7 +6,7 @@ from meetings.models import Meeting
 
 
 def welcome(request):
-    node_name = socket.gethostname()
+    node_name = socket.gethostbyaddr(socket.gethostname())[0]
     container_name = os.environ.get("HOSTNAME")
     return render(request, "website/welcome.html",
     {"node_name": node_name, "container_name" : container_name, "meetings": Meeting.objects.all()})
